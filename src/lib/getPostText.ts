80d80d35bdbdf7b06b97a735e6fe070ac6561830
kjakee <129994159+kjakee@users.noreply.github.com>
2024-11-30 07:39:42 +0000

    trying

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -5,13 +5,13 @@ const openai = new OpenAI();
 
 export default async function getPostText() {
   
-    const response = await axios.get('https://www.drivebc.ca/api/weather/observations/25221');
+    const response = await axios.get('http://api.mediastack.com/v1/news?access_key=cf61b6d8a9a692bd9c8ca8963a6f74f5&sources=cbc,ctv,global&limit=5');
     const users = response.data;
 
 
  const stream = await openai.chat.completions.create({
   model: "gpt-4o",
-  messages: [{ role: "user", content: "write a tweet from " + response.data + " and remove text DriveBC Alert. Include only hashtags #Coquihalla #CoquihallaWeather #CoquihallaSummit #CoquihallaRoadCondition #DriveCoquihalla"}],
+  messages: [{ role: "user", content: "write a tweet with 2 bullet points within 250 characters from " + response.data + " and remove text DriveBC Alert. Include only hashtags #Coquihalla #CoquihallaWeather #CoquihallaSummit #CoquihallaRoadCondition #DriveCoquihalla"}],
   stream: true,
 });
 var tweetString = '';
@@ -22,3 +22,4 @@ for await (const chunk of stream) {
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
   return tweetString;
 }
+
